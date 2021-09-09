9ab7819419adb608af3f2c3b160fd22ddd34a152
Viv Hafener <79525734+viv-codes@users.noreply.github.com>
2021-09-09 10:21:59 -0400

    DId most of the rest of the writing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import  date,datetime
 def calc():
     mealplan=input("Please enter the number corresponding to your meal plan: 1=$3500/semester 2=$3020/semester:")
     if(mealplan==1) {
-        funds=3500
+        funds=3500 #WHY DOESNT THIS WORK WTF PYTHON
         weekly=219
         daily=(weekly/7)
     } else {
@@ -15,7 +15,10 @@ def calc():
     d1= date(2021,8,movein)
     d2=date.today()
     totaldays=daysbetween(d1,d2)
-    totaldays
+    possiblespent=totaldays*daily
+    balence=input("Please enter how much funds you have remaining: ")
+    surplus=(funds-balence)-possiblespent
+    print("You have: ",surplus," dining dollars remaining. ")
 
 
 
