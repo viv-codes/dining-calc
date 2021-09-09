147937b470ff294c8fbf7ea9afe847b026491ad3
Viv Hafener <79525734+viv-codes@users.noreply.github.com>
2021-09-09 10:22:47 -0400

    syntax

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from datetime import  date,datetime
 
 def calc():
     mealplan=input("Please enter the number corresponding to your meal plan: 1=$3500/semester 2=$3020/semester:")
-    if(mealplan==1) {
+    if(mealplan==1):
         funds=3500 #WHY DOESNT THIS WORK WTF PYTHON
         weekly=219
         daily=(weekly/7)
-    } else {
+    else:
         funds=3020
         weekly=189
         daily=(weekly/7)
-    }
     movein=input("Please enter the day in August that you moved in: ")
     d1= date(2021,8,movein)
     d2=date.today()
