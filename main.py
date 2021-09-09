ec932175e53d8b3346fc10157daa98173f512be4
Viv Hafener <79525734+viv-codes@users.noreply.github.com>
2021-09-09 10:00:36 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from datetime import  date,datetime
+
+def calc():
+    mealplan=input("Please enter the number corresponding to your meal plan: 1=$3500/semester 2=$3020/semester:")
+    if(mealplan==1) {
+        funds=3500
+        weekly=219
+        daily=(weekly/7)
+    } else {
+        funds=3020
+        weekly=189
+        daily=(weekly/7)
+    }
+    movein=input("Please enter the day in August that you moved in: ")
+    d1= date(2021,8,movein)
+    d2=date.today()
+    totaldays=daysbetween(d1,d2)
+    totaldays
+
+
+
+def daysbetween(d1,d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
+
+def main():
+    calc()
+main()
