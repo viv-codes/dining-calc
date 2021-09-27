5482fc0b2b2f951a3e732b264bbfa1dd38da1a88
Viv Hafener <79525734+viv-codes@users.noreply.github.com>
2021-09-27 11:25:31 -0400

    Completed new changes

--- dining_calc.py
+++ dining_calc.py
@@ -1,7 +1,6 @@
 from datetime import date,datetime
 from main import daysbetween
 
-
 def calc():
     print("You have",int(input("Enter amount of dining dollars you have remaining: "))/daysbetween(date.today(),"2021-12-17"),"dining dollars per day for the rest of the semester.")
 
